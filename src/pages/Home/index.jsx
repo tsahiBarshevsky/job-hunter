5dc2960fe63323450b0bd6e2e48a751d76753529
tsahiBarshevsky <tsahi.13@gmail.com>
2023-02-12 15:21:30 +0200

    Right side outline

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react';
+import Emoji from 'react-emoji-render';
 import { Button, TextField, InputAdornment, Typography } from '@mui/material';
 import { MdOutlineAlternateEmail, MdLock, MdVisibility, MdVisibilityOff } from 'react-icons/md';
 import { FcGoogle } from 'react-icons/fc';
@@ -8,6 +9,8 @@ import { ThemeContext } from '../../utils/themeContext';
 import { Switch } from '../../components';
 import useStyles from './styles';
 import './homepage.sass';
+
+import { PulseLoader } from "react-spinners";
 
 // Firebase
 import { authentication } from '../../utils/firebase';
@@ -143,8 +146,9 @@ const HomePage = () => {
                                 type="submit"
                                 variant="contained"
                                 className={classes.button}
+                                disabled={disabled}
                             >
-                                Sign In
+                                {disabled ? <PulseLoader color='white' size={8} /> : "Sign In"}
                             </Button>
                             <Typography className={`or or-${theme}`}>
                                 <span className={`span span-${theme}`}>Or</span>
@@ -268,8 +272,9 @@ const HomePage = () => {
                                 type="submit"
                                 variant="contained"
                                 className={classes.button}
+                                disabled={disabled}
                             >
-                                Sign Up
+                                {disabled ? <PulseLoader color='white' size={8} /> : "Sign Up"}
                             </Button>
                             <Typography className={`or or-${theme}`}>
                                 <span className={`span span-${theme}`}>Or</span>
@@ -301,8 +306,18 @@ const HomePage = () => {
                     }
 
                 </div>
-            </div >
+            </div>
             <div className="right-container">
+                <div>
+                    <Typography className={classes.text}>Welcome to Job Hunter!</Typography>
+                    <Typography className={classes.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa sem, efficitur in blandit et, lobortis quis sem. Nunc rutrum elit quis diam lobortis cursus.</Typography>
+                </div>
+                <div>
+                    <Typography className={classes.text}>All Rights Reserved © {new Date().getFullYear()}</Typography>
+                    <Typography className={classes.text} variant="subtitle2">
+                        Coded with <Emoji text=":heart:" /> by Tsahi Barshavsky
+                    </Typography>
+                </div>
             </div>
         </div >
     )
